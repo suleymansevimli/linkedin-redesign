d14cd2ddd34b5d145d9790d8eccbb163cf48ab6f
suleyman <suleyman.sevimli1@gmail.com>
2021-01-17 15:52:50 +0300

    added the icons story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css"
 import "../styles/variables.css"
+import "./storybook.css"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- stories/Icon/Icon.stories.js
+++ stories/Icon/Icon.stories.js
@@ -1,21 +1,95 @@
 import React from "react";
+import {
+    AddMessage,
+    Apps,
+    ArrowUp,
+    Attach,
+    Close,
+    Comment,
+    Connection,
+    Connections,
+    Contacts,
+    Document,
+    Emoji,
+    Eye,
+    Filter,
+    Following,
+    Gif,
+    Group,
+    Hamburger,
+    Hashtag,
+    Home,
+    Image,
+    Info,
+    Job,
+    Like,
+    Message,
+    Network,
+    News,
+    Notification,
+    Options,
+    Pages,
+    Photo,
+    Plus,
+    Search,
+    Send,
+    Share,
+    Teammates,
+    Tick,
+    Video,
+    Work
+} from "../../components/icons";
 
 
 export default {
-    title: "Components/Styles",
+    title: "Components/Styles"
 }
 
 const Icon = () => {
     return (
-        <div>
-            <div>
-
-                <p>Add Connection</p>
-            </div>
+        <div className={"iconsContainer"}>
+            <AddMessage/>
+            <Apps/>
+            <ArrowUp/>
+            <Attach/>
+            <Close/>
+            <Comment/>
+            <Connection/>
+            <Connections/>
+            <Contacts/>
+            <Document/>
+            <Emoji/>
+            <Eye/>
+            <Filter/>
+            <Following/>
+            <Gif/>
+            <Group/>
+            <Hamburger/>
+            <Hashtag/>
+            <Home/>
+            <Image/>
+            <Info/>
+            <Job/>
+            <Like/>
+            <Message/>
+            <Network/>
+            <News/>
+            <Notification/>
+            <Options/>
+            <Pages/>
+            <Photo/>
+            <Plus/>
+            <Search/>
+            <Send/>
+            <Share/>
+            <Teammates/>
+            <Tick/>
+            <Video/>
+            <Work/>
         </div>
     )
 }
 
-export const  Icons = () => {
+export const Icons = () => {
     return <Icon/>
 }
