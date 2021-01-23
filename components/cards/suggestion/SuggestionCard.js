a15f2f00a2f6ddde1235e7bb2cf7034628eaf406
suleyman <suleyman.sevimli1@gmail.com>
2021-01-23 14:21:27 +0300

    suggestion card container edited

--- components/cards/suggestion/SuggestionCard.js
+++ components/cards/suggestion/SuggestionCard.js
@@ -8,16 +8,66 @@ import SuggestionCard from "../network/suggestion/SuggestionCard";
 const SuggestionCardContainer = () => {
 
     const users = [
-        {job:"Software Engineer ",cover:"http://localhost:3000/img/suggestion-cover.png",avatar: "http://localhost:3000/img/user/jane-cooper.png", name: "Jane Cooper"},
-        {job:"Software Engineer ",cover:"http://localhost:3000/img/suggestion-cover.png",avatar: "http://localhost:3000/img/user/guy-hawkings.png", name: "Guy Hawkings"},
-        {job:"Software Engineer ",cover:"http://localhost:3000/img/suggestion-cover.png",avatar: "http://localhost:3000/img/user/robert-fox.png", name: "Robert Fox"},
-        {job:"Software Engineer ",cover:"http://localhost:3000/img/suggestion-cover.png",avatar: "http://localhost:3000/img/user/jane-cooper.png", name: "Jane Cooper"},
-        {job:"Software Engineer ",cover:"http://localhost:3000/img/suggestion-cover.png",avatar: "http://localhost:3000/img/user/vladimir.png", name: "vladimir"},
-        {job:"Software Engineer ",cover:"http://localhost:3000/img/suggestion-cover.png",avatar: "http://localhost:3000/img/user/jane-cooper.png", name: "Jane Cooper"},
-        {job:"Software Engineer ",cover:"http://localhost:3000/img/suggestion-cover.png",avatar: "http://localhost:3000/img/user/guy-hawkings.png", name: "Guy Hawkings"},
-        {job:"Software Engineer ",cover:"http://localhost:3000/img/suggestion-cover.png",avatar: "http://localhost:3000/img/user/robert-fox.png", name: "Robert Fox"},
-        {job:"Software Engineer ",cover:"http://localhost:3000/img/suggestion-cover.png",avatar: "http://localhost:3000/img/user/jane-cooper.png", name: "Jane Cooper"},
-        {job:"Software Engineer ",cover:"http://localhost:3000/img/suggestion-cover.png",avatar: "http://localhost:3000/img/user/vladimir.png", name: "vladimir"}
+        {
+            job: "Software Engineer ",
+            cover: "http://localhost:3000/img/suggestion-cover.png",
+            avatar: "http://localhost:3000/img/user/jane-cooper.png",
+            name: "Jane Cooper"
+        },
+        {
+            job: "Software Engineer ",
+            cover: "http://localhost:3000/img/suggestion-cover.png",
+            avatar: "http://localhost:3000/img/user/guy-hawkings.png",
+            name: "Guy Hawkings"
+        },
+        {
+            job: "Software Engineer ",
+            cover: "http://localhost:3000/img/suggestion-cover.png",
+            avatar: "http://localhost:3000/img/user/robert-fox.png",
+            name: "Robert Fox"
+        },
+        {
+            job: "Software Engineer ",
+            cover: "http://localhost:3000/img/suggestion-cover.png",
+            avatar: "http://localhost:3000/img/user/jane-cooper.png",
+            name: "Jane Cooper"
+        },
+        {
+            job: "Software Engineer ",
+            cover: "http://localhost:3000/img/suggestion-cover.png",
+            avatar: "http://localhost:3000/img/user/vladimir.png",
+            name: "vladimir"
+        },
+        {
+            job: "Software Engineer ",
+            cover: "http://localhost:3000/img/suggestion-cover.png",
+            avatar: "http://localhost:3000/img/user/jane-cooper.png",
+            name: "Jane Cooper"
+        },
+        {
+            job: "Software Engineer ",
+            cover: "http://localhost:3000/img/suggestion-cover.png",
+            avatar: "http://localhost:3000/img/user/guy-hawkings.png",
+            name: "Guy Hawkings"
+        },
+        {
+            job: "Software Engineer ",
+            cover: "http://localhost:3000/img/suggestion-cover.png",
+            avatar: "http://localhost:3000/img/user/robert-fox.png",
+            name: "Robert Fox"
+        },
+        {
+            job: "Software Engineer ",
+            cover: "http://localhost:3000/img/suggestion-cover.png",
+            avatar: "http://localhost:3000/img/user/jane-cooper.png",
+            name: "Jane Cooper"
+        },
+        {
+            job: "Software Engineer ",
+            cover: "http://localhost:3000/img/suggestion-cover.png",
+            avatar: "http://localhost:3000/img/user/vladimir.png",
+            name: "vladimir"
+        }
     ]
 
 
@@ -30,14 +80,45 @@ const SuggestionCardContainer = () => {
                 </div>
 
                 <div className={style.suggestions}>
-                    <SuggestionCard user={users[0]} connections={"Nick Rybak and 131 others"} onDecline={() => alert("declined")} onAccept={() => alert("accepted")}/>
-                    <SuggestionCard user={users[1]} connections={"Nick Rybak and 131 others"} onDecline={() => alert("declined")} onAccept={() => alert("accepted")}/>
-                    <SuggestionCard user={users[2]} connections={"Nick Rybak and 131 others"} onDecline={() => alert("declined")} onAccept={() => alert("accepted")}/>
-                    <SuggestionCard user={users[3]} connections={"Nick Rybak and 131 others"} onDecline={() => alert("declined")} onAccept={() => alert("accepted")}/>
-                    <SuggestionCard user={users[4]} connections={"Nick Rybak and 131 others"} onDecline={() => alert("declined")} onAccept={() => alert("accepted")}/>
-                    <SuggestionCard user={users[5]} connections={"Nick Rybak and 131 others"} onDecline={() => alert("declined")} onAccept={() => alert("accepted")}/>
-                    <SuggestionCard user={users[6]} connections={"Nick Rybak and 131 others"} onDecline={() => alert("declined")} onAccept={() => alert("accepted")}/>
-                    <SuggestionCard user={users[7]} connections={"Nick Rybak and 131 others"} onDecline={() => alert("declined")} onAccept={() => alert("accepted")}/>
+                    <SuggestionCard user={users[0]}
+                                    connections={"Nick Rybak and 131 others"}
+                                    onDecline={() => alert("declined")}
+                                    onAccept={() => alert("accepted")}/>
+
+                    <SuggestionCard user={users[1]}
+                                    connections={"Nick Rybak and 131 others"}
+                                    onDecline={() => alert("declined")}
+                                    onAccept={() => alert("accepted")}/>
+
+                    <SuggestionCard user={users[2]}
+                                    connections={"Nick Rybak and 131 others"}
+                                    onDecline={() => alert("declined")}
+                                    onAccept={() => alert("accepted")}/>
+
+                    <SuggestionCard user={users[3]}
+                                    connections={"Nick Rybak and 131 others"}
+                                    onDecline={() => alert("declined")}
+                                    onAccept={() => alert("accepted")}/>
+
+                    <SuggestionCard user={users[4]}
+                                    connections={"Nick Rybak and 131 others"}
+                                    onDecline={() => alert("declined")}
+                                    onAccept={() => alert("accepted")}/>
+
+                    <SuggestionCard user={users[5]}
+                                    connections={"Nick Rybak and 131 others"}
+                                    onDecline={() => alert("declined")}
+                                    onAccept={() => alert("accepted")}/>
+
+                    <SuggestionCard user={users[6]}
+                                    connections={"Nick Rybak and 131 others"}
+                                    onDecline={() => alert("declined")}
+                                    onAccept={() => alert("accepted")}/>
+
+                    <SuggestionCard user={users[7]}
+                                    connections={"Nick Rybak and 131 others"}
+                                    onDecline={() => alert("declined")}
+                                    onAccept={() => alert("accepted")}/>
                 </div>
 
             </div>
